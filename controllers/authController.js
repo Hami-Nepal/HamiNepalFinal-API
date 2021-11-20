ae8032e09132846b3094e1c8718cfdd32eb0bf4f
SudeepMI <sudeepm0599@gmail.com>
2021-11-20 16:51:09 +0545

    login validation

--- controllers/authController.js
+++ controllers/authController.js
@@ -364,22 +364,22 @@ exports.login = catchAsync(async (req, res, next) => {
 });
 
 exports.loginAdmin = catchAsync(async (req, res, next) => {
-  const schema = Joi.object({
-    password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
-    email: Joi.string().email({
-      minDomainSegments: 2,
-      tlds: { allow: ["com", "net"] },
-    }),
-  });
+  // const schema = Joi.object({
+  //   password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
+  //   email: Joi.string().email({
+  //     minDomainSegments: 2,
+  //     tlds: { allow: ["com", "net"] },
+  //   }),
+  // });
 
-  const { error } = schema.validate({
-    email: req.body.email,
-    password: req.body.password,
-  });
+  // const { error } = schema.validate({
+  //   email: req.body.email,
+  //   password: req.body.password,
+  // });
 
-  if (error) {
-    return next(new AppError(`${error.details[0].message}`, 403));
-  }
+  // if (error) {
+  //   return next(new AppError(`${error.details[0].message}`, 403));
+  // }
 
   const { email, password } = req.body;
 
