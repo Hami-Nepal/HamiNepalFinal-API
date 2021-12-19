9385a2a2ad1c07821a2c6606338184cd4c5da5ac
pawan3s <pawan29s123@gmail.com>
2021-12-19 12:28:16 +0545

    ...

--- controllers/boardMemberController.js
+++ controllers/boardMemberController.js
@@ -29,7 +29,6 @@ const upload = multer({
 exports.uploadBoardMemberPhoto = upload.single("photo");
 
 exports.resizeBoardMemberPhoto = catchAsync(async (req, res, next) => {
-  console.log(req.file);
   if (!req.file) return next();
 
   req.file.filename = `boardmember-${Date.now()}.jpeg`;
